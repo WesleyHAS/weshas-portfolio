e277dcd47a35477441ca709764f75f407e5115fe
Wes <wesley.has@outlook.com>
2023-11-29 16:36:26 -0700

    fixed portfolio link

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,54 +1,17 @@
-// import React, { useState } from "react";
-// import { Document, Page } from "react-pdf";
-// import MyDocument from "../assets/documents/resumeWesley.pdf";
+import React from "react";
 
-// export default function Resume() {
-//   const [numPages, setNumPages] = useState(null);
-//   function onDocumentSuccess({ numPages }) {
-//     setNumPages(numPages);
-//   }
-//   return (
-//     <div style={{ display: "flex", justifyContent: "center" }}>
-//       <div style={{ width: "700px", border: "3px solid gray" }}>
-//         <Document file={MyDocument} onLoadSuccess={onDocumentSuccess}>
-//           {Array(numPages)
-//             .fill()
-//             .map((_, i) => (
-//               <Page pageNumber={i + 1}></Page>
-//             ))}
-//         </Document>
-//       </div>
-//     </div>
-//   );
-// }
-
-import React, { useState } from "react";
-import { Document, Page } from "react-pdf";
-
-export default function Resume() {
-  const [numPages, setNumPages] = useState(null);
-
-  function onDocumentSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
+const Resume = () => {
+  const fileUrl =
+    "https://raw.githubusercontent.com/WesleyHAS/weshas-portfolio/main/src/assets/documents/resumeWesley.pdf";
 
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <div style={{ width: "700px", border: "3px solid gray" }}>
-        {!numPages && <p>Loading...</p>}
-        {numPages && (
-          <Document
-            file="https://example.com/path/to/resumeWesley.pdf"
-            onLoadSuccess={onDocumentSuccess}
-          >
-            {Array(numPages)
-              .fill()
-              .map((_, i) => (
-                <Page key={i + 1} pageNumber={i + 1} />
-              ))}
-          </Document>
-        )}
-      </div>
+    <div>
+      <p>Click the link below to download the file:</p>
+      <a href={fileUrl} download="resumeWesley.pdf">
+        Download Resume
+      </a>
     </div>
   );
-}
+};
+
+export default Resume;
