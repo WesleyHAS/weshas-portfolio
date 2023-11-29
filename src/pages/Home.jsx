f5fb75006e2fc3a4cc598315571c3606e4f92e7e
Wes <wesley.has@outlook.com>
2023-11-29 10:43:28 -0700

    added placeholder content for home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,37 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
+    <div class="main-containers">
+      <p id="about-me" class="left-container">
+        Home
+        <br />
+        Page
       </p>
+      <section class="right-container">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Viverra ipsum nunc
+        aliquet bibendum. Ullamcorper a lacus vestibulum sed arcu non odio
+        euismod. Faucibus ornare suspendisse sed nisi lacus sed viverra tellus
+        in. Gravida arcu ac tortor dignissim convallis. Pretium quam vulputate
+        dignissim suspendisse in. Sagittis orci a scelerisque purus semper eget
+        duis. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit
+        sed. Felis imperdiet proin fermentum leo vel. Turpis tincidunt id
+        aliquet risus feugiat in ante. Diam donec adipiscing tristique risus.
+        Sit amet dictum sit amet justo donec.
+        <br />
+        <br />
+        Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Tempus egestas sed sed
+        risus pretium. Non consectetur a erat nam at. Suscipit adipiscing
+        bibendum est ultricies integer quis auctor elit sed. Dui sapien eget mi
+        proin sed libero. Quam adipiscing vitae proin sagittis nisl rhoncus
+        mattis rhoncus. Varius quam quisque id diam vel. Mattis ullamcorper
+        velit sed ullamcorper morbi tincidunt ornare. Faucibus turpis in eu mi
+        bibendum neque egestas congue quisque. Nulla pharetra diam sit amet.
+        Malesuada nunc vel risus commodo viverra maecenas accumsan. Egestas
+        purus viverra accumsan in nisl. Ut diam quam nulla porttitor massa id.
+        Facilisis volutpat est velit egestas dui id ornare. Mauris rhoncus
+        aenean vel elit scelerisque mauris. Justo donec enim diam vulputate.
+      </section>
     </div>
   );
 }
